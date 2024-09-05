84f6b54fcd529345385dd62aaa4a90edf358e49f
Vithushayini <vithushayini2000@gmail.com>
2024-09-05 21:26:18 +0530

    #4.2.1, Validate invalid username or password on the Login screen

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,7 +5,43 @@ import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 export default function LoginPage(){
-    
+    const initialStateErrors = {
+        username:null,
+        password:null
+    };
+    const [errors,setErrors]=useState(initialStateErrors);
+
+    const [error,setError]=useState(null);
+
+    const [loading,setLoading]=useState(false);
+
+    const [inputs,setInputs]=useState({
+        username:"",
+        password:""
+    })
+
+    const navigate = useNavigate();
+
+    const handleInput= (event)=>{
+        setInputs({...inputs,[event.target.name]:event.target.value})
+    }
+
+    const handleSubmit=async (event)=>{
+        event.preventDefault();
+        let errors =initialStateErrors; 
+        // setErrors(initialStateErrors);
+        
+        if(inputs.username == ""){
+            errors.username="username required";
+        }
+        if (inputs.password == "") {
+            errors.password="password required";
+        }
+
+        setErrors({...errors});
+        
+    }
+
 
     return (
         <div className="login-page-container">
@@ -13,27 +49,55 @@ export default function LoginPage(){
                 <div className="login-container">
                     <div className="login-content">
                         <h2 className="login-title">Login Now</h2>
-                        <form className="login-form">
+                        <form onSubmit={handleSubmit} className="login-form">
                             <div className="form-group">
                                 <label htmlFor="username" className="login-label">Username</label>
                                 <input
                                     type="text"
                                     className="login-input"
+                                    onChange={handleInput}
                                     name="username"
                                     placeholder="Enter your username"
                                 />
+                                 { errors.username?
+                                (<span className="error-message" >
+                                    {errors.username}
+                                </span>):null
+                                }
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password" className="login-label">Password</label>
                                 <input
                                     type="password"
                                     className="login-input"
+                                    onChange={handleInput}
                                     name="password"
                                     placeholder="Enter your password"
                                 />
+                               
+                                { errors.password?
+                                (<span className="error-message" >
+                                    {errors.password}
+                                </span>):null
+                                }
                             </div>
                             <div className="form-group">
-                                <input type="submit" className="login-btn" value="Login" />
+                            {loading ?
+                                (<div  className="loading-container">
+                                    <div className="loading-spinner">
+                                        <span className="sr-only">Loading...</span>
+                                    </div>
+                                </div>):null
+                                }
+                                <span className="error-message" >
+                                { error?
+                                (<p>{error}</p>)
+                                :null
+                                }
+                                </span>
+                                 
+
+                                <input type="submit" className="login-btn" disabled={loading} value="Login" />
                             </div>
                         </form>
                     </div>
