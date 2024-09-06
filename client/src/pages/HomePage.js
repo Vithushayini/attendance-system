a031f8adec7c5354cb707f26a6487b14d2aae54e
Vithushayini <vithushayini2000@gmail.com>
2024-09-06 06:45:10 +0530

    #4.2.3,Redirect to the home page on Logout

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,19 @@
 import '../pages/HomePage.css';
 
+import {useNavigate} from 'react-router-dom';
+
 export default function HomePage(){
+    const navigate = useNavigate();
+
+    const handleLogout = ()=>{
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+
     return(
         <div className="home-container">
             <h1 className="home-title">Welcome to home page</h1>
-            <button className="logout-button">Logout</button>
+            <button onClick={handleLogout} className="logout-button">Logout</button>
         </div>
         
 
